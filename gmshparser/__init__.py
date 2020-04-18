957b7202b3e847cdf902b68416f1ba792c76ef78
Jukka Aho <ahojukka5@gmail.com>
2020-04-19 03:15:02 +0700

    Add NodesParser
    Parses section $Nodes ... $EndNodes
    
    Implements classes:
    - NodesParser
    - Node
    - NodeEntity

--- gmshparser/__init__.py
+++ gmshparser/__init__.py
@@ -1,4 +1,5 @@
-from typing import List, TextIO
+from typing import List, Tuple, TextIO
+
 
 def parse_ints(io: TextIO) -> List[int]:
     """Parse first line of io to list of integers.
@@ -25,9 +26,10 @@ def parse_ints(io: TextIO) -> List[int]:
     ints = map(int, parts)
     return list(ints)
 
+
 def parse_floats(io: TextIO) -> List[float]:
     """Parse first line of io to list of floats.
-    
+
     Parameters
     ----------
     io :: TextIO
@@ -50,6 +52,84 @@ def parse_floats(io: TextIO) -> List[float]:
     ints = map(float, parts)
     return list(ints)
 
+
+class Node(object):
+
+    """ Node. """
+
+    def __init__(self):
+        self.tag_ = -1
+        self.coordinates_ = (None, None, None)
+
+    def set_tag(self, tag: int):
+        """Set node tag (node id)."""
+        self.tag_ = tag
+
+    def get_tag(self) -> int:
+        """Get node tag (node id)."""
+        return self.tag_
+
+    def set_coordinates(self, coordinates: Tuple[float, float, float]):
+        """Set the coordinates of the node."""
+        self.coordinates_ = coordinates
+
+    def get_coordinates(self) -> Tuple[float, float, float]:
+        """Get the coordinates of the node."""
+        return self.coordinates_
+
+
+class NodeEntity(object):
+
+    """ NodeEntity class holds nodes for one block. """
+
+    def __init__(self):
+        self.dimension_ = -1
+        self.tag_ = -1
+        self.number_of_parametric_coordinates_ = -1
+        self.number_of_nodes_ = -1
+        self.nodes_ = {}
+
+    def set_dimension(self, dimension: int):
+        """Set the dimension of the entity to `dimension`."""
+        self.dimension_ = dimension
+
+    def get_dimension(self) -> int:
+        """Get the dimension of the entity."""
+        return self.dimension_
+
+    def set_tag(self, tag: int):
+        """Set the tag of the entity."""
+        self.tag_ = tag
+
+    def get_tag(self) -> int:
+        """Get the tag of the entity."""
+        return self.tag_
+
+    def set_number_of_parametric_coordinates(self, npar: int):
+        """Set the number of parametric coordinates of the entity."""
+        self.number_of_parametric_coordinates_ = npar
+
+    def get_number_of_parametric_coordinates(self) -> int:
+        """Get the number of parametric coordinates of the entity."""
+        return self.number_of_parametric_coordinates_
+
+    def set_number_of_nodes(self, number_of_nodes: int):
+        """Set the number of nodes of the entity."""
+        self.number_of_nodes_ = number_of_nodes
+
+    def get_number_of_nodes(self) -> int:
+        """Get the number of nodes of the entity."""
+        return self.number_of_nodes_
+
+    def add_node(self, node: Node):
+        """Add new node to entity."""
+        self.nodes_[node.get_tag()] = node
+
+    def get_node(self, tag: int):
+        """Get node from entity by its tag."""
+        return self.nodes_[tag]
+
+
 class Mesh(object):
 
     """ Mesh is the main object of the package, containing the Gmsh .msh information. """
@@ -57,7 +137,12 @@ class Mesh(object):
     def __init__(self):
         self.version_ = "unknown"
         self.ascii_ = False
-        self.precision_ = -1 # t_size
+        self.precision_ = -1  # t_size
+        self.number_of_entities_ = -1
+        self.number_of_nodes_ = -1
+        self.min_node_tag_ = -1
+        self.max_node_tag_ = -1
+        self.entities_ = {}
 
     def set_version(self, version: str):
         """Set the version of the Mesh object"""
@@ -83,8 +168,49 @@ class Mesh(object):
         """Get the precision of the mesh"""
         return self.precision_
 
+    def set_number_of_entities(self, number_of_entities: int):
+        """Set number of entities."""
+        self.number_of_entities_ = number_of_entities
 
+    def get_number_of_entities(self) -> int:
+        """Get number of entities."""
+        return self.number_of_entities_
 
+    def set_number_of_nodes(self, number_of_nodes: int):
+        """Set number of nodes."""
+        self.number_of_nodes_ = number_of_nodes
+
+    def get_number_of_nodes(self) -> int:
+        """Get number of nodes."""
+        return self.number_of_nodes_
+
+    def set_min_node_tag(self, min_node_tag: int):
+        """Set node minimum tag."""
+        self.min_node_tag_ = min_node_tag
+
+    def get_min_node_tag(self) -> int:
+        """Get node minimum tag."""
+        return self.min_node_tag_
+
+    def set_max_node_tag(self, max_node_tag: int):
+        """Set node maximum tag."""
+        self.max_node_tag_ = max_node_tag
+
+    def get_max_node_tag(self) -> int:
+        """Get node maximum tag."""
+        return self.max_node_tag_
+
+    def has_node_entity(self, entity_tag) -> bool:
+        """Test does mesh have entity with `entity_tag`."""
+        return entity_tag in self.entities_
+
+    def add_node_entity(self, entity: NodeEntity):
+        """Add node entity to mesh."""
+        self.entities_[entity.get_tag()] = entity
+
+    def get_node_entity(self, tag: int):
+        """Get node entity based on tag."""
+        return self.entities_[tag]
 
 class AbstractParser(object):
     def parse(self, mesh, io):
@@ -101,17 +227,39 @@ class MeshFormatParser(AbstractParser):
 
 
 class NodesParser(AbstractParser):
+
     def parse(self, mesh, io):
-        s = io.readline().split(" ")
-        number_of_entities = int(s[0])
-        number_of_nodes = int(s[1])
-        node_min_label = int(s[2])
-        node_max_label = int(s[3])
+        meta = parse_ints(io)
+        mesh.set_number_of_entities(meta[0])
+        mesh.set_number_of_nodes(meta[1])
+        mesh.set_min_node_tag(meta[2])
+        mesh.set_max_node_tag(meta[3])
+        for i in range(mesh.get_number_of_entities()):
+            emeta = parse_ints(io)
+            entity = NodeEntity()
+            entity.set_dimension(emeta[0])
+            entity.set_tag(emeta[1])
+            entity.set_number_of_parametric_coordinates(emeta[2])
+            entity.set_number_of_nodes(emeta[3])
+            node_tags: List[int] = []
+            for j in range(entity.get_number_of_nodes()):
+                tag = int(io.readline())
+                node = Node()
+                node.set_tag(tag)
+                node_tags.append(tag)
+                entity.add_node(node)
+            for tag in node_tags:
+                coordinates = parse_floats(io)
+                node = entity.get_node(tag)
+                node.set_coordinates(tuple(coordinates))
+            mesh.add_node_entity(entity)
 
 
 PARSERS = {
     "$MeshFormat": MeshFormatParser,
+    "$Nodes": NodesParser
 }
+
 
 def parse_io(io, parsers=PARSERS):
     mesh = Mesh()
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -2,6 +2,7 @@ import gmshparser
 import io
 import pytest
 
+
 @pytest.fixture
 def mesh():
     data = """
@@ -55,3 +56,28 @@ def test_version(mesh):
     assert mesh.get_ascii()
     assert mesh.get_precision() == 8
 
+
+def test_nodes(mesh):
+    assert mesh.get_number_of_nodes() == 6
+    assert mesh.get_min_node_tag() == 1
+    assert mesh.get_max_node_tag() == 6
+
+    assert mesh.has_node_entity(1)
+    entity = mesh.get_node_entity(1)
+    assert entity.get_tag() == 1
+    assert entity.get_dimension() == 2
+    assert entity.get_number_of_parametric_coordinates() == 0
+    assert entity.get_number_of_nodes() == 6
+
+    assert entity.get_node(1).get_tag() == 1
+    assert entity.get_node(1).get_coordinates() == (0.0, 0.0, 0.0)
+    assert entity.get_node(2).get_tag() == 2
+    assert entity.get_node(2).get_coordinates() == (1.0, 0.0, 0.0)
+    assert entity.get_node(3).get_tag() == 3
+    assert entity.get_node(3).get_coordinates() == (1.0, 1.0, 0.0)
+    assert entity.get_node(4).get_tag() == 4
+    assert entity.get_node(4).get_coordinates() == (0.0, 1.0, 0.0)
+    assert entity.get_node(5).get_tag() == 5
+    assert entity.get_node(5).get_coordinates() == (2.0, 0.0, 0.0)
+    assert entity.get_node(6).get_tag() == 6
+    assert entity.get_node(6).get_coordinates() == (2.0, 1.0, 0.0)
