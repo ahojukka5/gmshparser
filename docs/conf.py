27dbfacba9220821c4f56239c9aa63bce1f89209
Jukka Aho <ahojukka5@gmail.com>
2020-04-19 01:52:33 +0700

    Add master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,7 @@ copyright = '2020, Jukka Aho'
 author = 'Jukka Aho'
 release = '0.1.0'
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'recommonmark']
+master_doc = 'index'
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 html_theme = 'sphinx_rtd_theme'
