df8a398f6fda5fdcea35b399d28d4834b07fac5a
Jukka Aho <ahojukka5@gmail.com>
2020-05-09 00:27:34 +0700

    Add tests for ElementEntity

---
+++ tests/test_element_entity.py
@@ -0,0 +1,24 @@
+from gmshparser.element_entity import ElementEntity
+
+
+class ElementMock(object):
+    def __init__(self):
+        pass
+
+    def get_tag(self):
+        return 42
+
+
+def test_element_entity():
+    ee = ElementEntity()
+    ee.set_dimension(2)
+    ee.set_tag(1)
+    ee.set_number_of_elements(1)
+    ee.set_element_type(3)
+    element = ElementMock()
+    ee.add_element(element)
+    assert ee.get_dimension() == 2
+    assert ee.get_tag() == 1
+    assert ee.get_number_of_elements() == 1
+    assert ee.get_element_type() == 3
+    assert ee.get_element(42) == element
