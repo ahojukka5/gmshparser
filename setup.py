5126a350fe4656ace5f9fe8147d4f0de22621759
Jukka Aho <ahojukka5@gmail.com>
2020-05-09 07:18:19 +0700

    Add better short description

--- setup.py
+++ setup.py
@@ -15,12 +15,18 @@ extras = {
     'test': test_deps,
 }
 
+description = """
+gmshparser is a lightweight and well-tested package that aims to reliably parse
+the Gmsh ascii file format (.msh). The package does not introduce any external
+dependencies and thus fits well with the needs of your own FEM research code.
+"""
+
 setuptools.setup(
     name="gmshparser",
     version="0.1.0",
     author="Jukka Aho",
     author_email="ahojukka5@gmail.com",
-    description="Package is used to parser Gmsh .msh file format",
+    description=description,
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ahojukka5/gmshparser",
