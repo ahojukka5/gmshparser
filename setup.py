3015c600f782e41ca696263f627db50d782669de
Jukka Aho <ahojukka5@gmail.com>
2020-05-09 07:18:46 +0700

    Get version info from __init__.py

--- setup.py
+++ setup.py
@@ -1,5 +1,26 @@
 import setuptools
 
+# https://packaging.python.org/guides/single-sourcing-package-version/
+
+import codecs
+import os.path
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
@@ -23,7 +44,7 @@ dependencies and thus fits well with the needs of your own FEM research code.
 
 setuptools.setup(
     name="gmshparser",
-    version="0.1.0",
+    version=get_version("gmshparser/__init__.py"),
     author="Jukka Aho",
     author_email="ahojukka5@gmail.com",
     description=description,
