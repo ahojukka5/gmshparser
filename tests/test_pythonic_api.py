0d0643c291c0c72163593e2890a174c2a673407b
Jukka Aho <ahojukka5@gmail.com>
2026-07-24 20:33:32 +0300

    Test the refined Pythonic API model

--- tests/test_pythonic_api.py
+++ tests/test_pythonic_api.py
@@ -1,7 +1,10 @@
+from dataclasses import FrozenInstanceError
 from io import StringIO
 
+import pytest
+
 import gmshparser
-from gmshparser.api import Mesh as ModernMesh
+from gmshparser.api import ElementType, Mesh as ModernMesh, Version
 
 MESH = """$MeshFormat
 4.1 0 8
@@ -30,16 +33,31 @@ $Elements
 $EndElements
 """
 
+PARAMETRIC_MESH = """$MeshFormat
+4.1 0 8
+$EndMeshFormat
+$Nodes
+1 1 1 1
+2 1 1 1
+1
+0.0 0.0 0.0 0.25 0.75
+$EndNodes
+$Elements
+0 0 0 0
+$EndElements
+"""
+
 
 def test_read_stream_exposes_flat_pythonic_collections():
     mesh = gmshparser.read(StringIO(MESH), name="memory.msh")
 
     assert isinstance(mesh, ModernMesh)
-    assert repr(mesh) == "Mesh(name='memory.msh', version=4.1, nodes=6, elements=2)"
-    assert mesh.version == 4.1
-    assert mesh.version_info == (4, 1)
+    assert mesh.version == Version(4, 1)
+    assert str(mesh.version) == "4.1"
+    assert float(mesh.version) == 4.1
     assert mesh.is_ascii is True
-    assert mesh.precision == 8
+    assert mesh.data_size == 8
+    assert mesh.dimension == 2
 
     assert len(mesh.nodes) == 6
     assert mesh.nodes.tags == (1, 2, 3, 4, 5, 6)
@@ -52,37 +70,72 @@ def test_read_stream_exposes_flat_pythonic_collections():
         0.0,
     )
     assert tuple(mesh.nodes[2]) == (1.0, 0.0, 0.0)
+    assert mesh.nodes[2].entity_key == (2, 1)
 
     assert len(mesh.elements) == 2
     assert mesh.elements.tags == (1, 2)
-    assert mesh.elements.types == frozenset({3})
-    assert mesh.elements[1].node_tags == (1, 2, 3, 4)
-    assert mesh.elements[1].connectivity == (1, 2, 3, 4)
-    assert tuple(mesh.elements[1]) == (1, 2, 3, 4)
-    assert len(mesh.elements[1]) == 4
+    assert mesh.elements.types == frozenset({ElementType.QUADRANGLE})
+    element = mesh.elements[1]
+    assert element.type is ElementType.QUADRANGLE
+    assert element.element_type is ElementType.QUADRANGLE
+    assert element.type_id == 3
+    assert element.node_tags == (1, 2, 3, 4)
+    assert element.connectivity == (1, 2, 3, 4)
+    assert tuple(node.tag for node in element) == (1, 2, 3, 4)
+    assert element.nodes[0] is mesh.nodes[1]
+    assert element.entity_key == (2, 1)
+    assert len(element) == 4
 
 
-def test_filtering_and_entity_access_share_value_objects():
+def test_filtering_and_unified_entity_access_share_value_objects():
     mesh = gmshparser.read(StringIO(MESH))
 
-    surface_nodes = mesh.nodes.where(dimension=2, entity_tag=1)
+    surface_nodes = mesh.nodes.where(entity=(2, 1))
     quads = mesh.elements.where(
-        element_type=3,
-        dimension=2,
-        entity_tag=1,
+        element_type=ElementType.QUADRANGLE,
+        entity=(2, 1),
     )
 
     assert surface_nodes.tags == (1, 2, 3, 4, 5, 6)
     assert quads.tags == (1, 2)
     assert mesh.elements.by_type(3) == quads
 
-    node_entity = mesh.node_entities[(2, 1)]
-    element_entity = mesh.element_entities[(2, 1)]
-    assert len(node_entity) == 6
-    assert len(element_entity) == 2
-    assert node_entity.nodes[1] is mesh.nodes[1]
-    assert element_entity.elements[1] is mesh.elements[1]
-    assert mesh.node_entities.where(dimension=2).keys == ((2, 1),)
+    entity = mesh.entities[(2, 1)]
+    assert entity.key == (2, 1)
+    assert len(entity.nodes) == 6
+    assert len(entity.elements) == 2
+    assert entity.nodes[1] is mesh.nodes[1]
+    assert entity.elements[1] is mesh.elements[1]
+    assert entity.element_types == frozenset({ElementType.QUADRANGLE})
+    assert mesh.entities.where(dimension=2).keys == ((2, 1),)
+    assert mesh.entities.where(element_type=3).keys == ((2, 1),)
+
+
+def test_nodes_and_mesh_values_are_immutable():
+    mesh = gmshparser.read(StringIO(MESH))
+
+    with pytest.raises(FrozenInstanceError):
+        mesh.nodes[1].tag = 99
+
+    with pytest.raises(FrozenInstanceError):
+        mesh.name = "other.msh"
+
+
+def test_parametric_coordinates_are_separated_from_cartesian_coordinates():
+    mesh = gmshparser.read(StringIO(PARAMETRIC_MESH))
+    node = mesh.nodes[1]
+
+    assert node.coordinates == (0.0, 0.0, 0.0)
+    assert node.parametric_coordinates == (0.25, 0.75)
+    assert node.is_parametric is True
+    assert mesh.entities[(2, 1)].has_parametric_nodes is True
+
+
+def test_element_type_accepts_unnamed_gmsh_values():
+    element_type = ElementType(92)
+
+    assert int(element_type) == 92
+    assert element_type.name == "TYPE_92"
 
 
 def test_mesh_bounds_are_computed_from_nodes():
