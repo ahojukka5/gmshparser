07e35d28cecace6bffd23b716b9c70925763c5ff
Jukka Aho <ahojukka5@gmail.com>
2026-07-25 18:22:12 +0300

    Type legacy parametric node coordinates accurately

--- gmshparser/node.py
+++ gmshparser/node.py
@@ -6,10 +6,7 @@ class Node:
 
     def __init__(self) -> None:
         self.tag_ = -1
-        self.coordinates_ = cast(
-            tuple[float, float, float],
-            (None, None, None),
-        )
+        self.coordinates_ = cast(tuple[float, ...], (None, None, None))
 
     def set_tag(self, tag: int) -> None:
         """Set node tag (node id)."""
@@ -19,10 +16,10 @@ class Node:
         """Get node tag (node id)."""
         return self.tag_
 
-    def set_coordinates(self, coordinates: tuple[float, float, float]) -> None:
-        """Set the coordinates of the node."""
+    def set_coordinates(self, coordinates: tuple[float, ...]) -> None:
+        """Set Cartesian and optional parametric node coordinates."""
         self.coordinates_ = coordinates
 
-    def get_coordinates(self) -> tuple[float, float, float]:
-        """Get the coordinates of the node."""
+    def get_coordinates(self) -> tuple[float, ...]:
+        """Get Cartesian and optional parametric node coordinates."""
         return self.coordinates_
