e9fea28fa5850ae0138d2a44bcc1345ee3eba98a
Jukka Aho <ahojukka5@gmail.com>
2026-07-25 13:29:12 +0300

    Apply Ruff formatting to entities parser

--- gmshparser/entities_parser.py
+++ gmshparser/entities_parser.py
@@ -50,9 +50,7 @@ class EntitiesParser(AbstractParser):
 
                 try:
                     tag = int(parts[0])
-                    _ = tuple(
-                        float(value) for value in parts[1:physical_count_index]
-                    )
+                    _ = tuple(float(value) for value in parts[1:physical_count_index])
                     number_of_physical_tags = int(parts[physical_count_index])
                 except ValueError as error:
                     raise InvalidSectionError(
