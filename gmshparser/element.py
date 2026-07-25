1e8a656e9adafd9027785b18957be0b018dc68e6
Jukka Aho <ahojukka5@gmail.com>
2026-07-25 18:21:32 +0300

    Fully type the compatibility element model

--- gmshparser/element.py
+++ gmshparser/element.py
@@ -1,19 +1,19 @@
 class Element:
     """Element."""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.tag_ = -1
-        self.connectivity_ = []
+        self.connectivity_: list[int] = []
 
-    def set_tag(self, tag: int):
+    def set_tag(self, tag: int) -> None:
         """Set element tag."""
         self.tag_ = tag
 
-    def get_tag(self):
+    def get_tag(self) -> int:
         """Get element tag."""
         return self.tag_
 
-    def set_connectivity(self, connectivity: list[int]):
+    def set_connectivity(self, connectivity: list[int]) -> None:
         """Set element connectivity."""
         self.connectivity_ = connectivity
 
